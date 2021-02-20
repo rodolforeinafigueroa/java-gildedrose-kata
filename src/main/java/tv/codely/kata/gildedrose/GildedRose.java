1dae4c5ba357fa656fb3fc861bd34dfd60f7b4cd
rodolforeinafigueroa <rodolfo.reina@outlook.com>
2021-02-20 08:27:24 -0500

    chache for to foreach

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -10,11 +10,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-
-            item.update();
-
-        }
+        items.forEach(Item::update); 
     }
 
 }
